71711c711f590f2796315bb8019c20b47654652d
Jeremy Cheung <cmcheung001@gmail.com>
2024-01-27 10:38:06 +0800

    check out terraform files 27 Jan 2024 10:36

--- main.tf
+++ main.tf
@@ -4,4 +4,17 @@ provider "aws" {
 }
 
 
+resource "aws_sns_topic" "user_updates" {
+  name = "jeremy001-updates-topic"
+}
+
+
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce5-tfstate-bucket-1"
+    key    = "jeremy.tfstate"
+    region = "us-east-1"
+  }
+}
 
