906e0f25ac2f7776448a4ef8ccafd4584b3c96a0
Jeremy Cheung <cmcheung001@gmail.com>
2024-02-17 12:58:53 +0800

    test again

--- main.tf
+++ main.tf
@@ -1,20 +1,10 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 
 resource "aws_sns_topic" "user_updates" {
-  name = "jeremy001-updates-topic"
+  provider = aws.dev
+  name = var.res_name + "-updates-topic"
+  #name = "jeremy001-updates-topic"
 }
 
 
 
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce5-tfstate-bucket-1"
-    key    = "jeremy.tfstate"
-    region = "us-east-1"
-  }
-}
 
---
+++ provider.tf
@@ -0,0 +1,6 @@
+# Configure the AWS Provider
+provider "aws" {
+ # alias = "dev"
+ region = "us-east-1"
+}
+
