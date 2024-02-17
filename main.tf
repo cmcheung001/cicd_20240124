3698060bed7280f70045dacc628d6fc9870b8778
Jeremy Cheung <cmcheung001@gmail.com>
2024-02-17 13:01:02 +0800

    test test

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_sns_topic" "user_updates" {
-  provider = aws.dev
-  name = var.res_name + "-updates-topic"
+  provider = aws
+  name = var.res_name # + "-updates-topic"
   #name = "jeremy001-updates-topic"
 }
 
